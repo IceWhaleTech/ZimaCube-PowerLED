82e10dbbde8c86488aafaeb8f02db27b887b0634
Link <a624669980@163.com>
2024-07-02 07:40:58 +0000

    add led off status

--- powerled.c
+++ powerled.c
@@ -10,17 +10,20 @@
 #define LDNREG 0x07
 #define GPIOLDN 0x07
 #define CMDF9 0xF9
+#define CMDF8 0xF8
 
 #define CMD66 0x66
 #define CMD62 0x62
-#define ADR   0xF8
+#define ADR 0xF8
 #define CMD81 0x81
 #define CMD80 0x80
 
 //-------------------n100 start-------------------
 
-void enter_conf_mode() {
-    if (iopl(3)) {
+void enter_conf_mode()
+{
+    if (iopl(3))
+    {
         fprintf(stderr, "Can't get IO permissions\n");
         exit(1);
     }
@@ -30,31 +33,37 @@ void enter_conf_mode() {
     outb(0x55, REG);
 }
 
-void select_ldn(unsigned char ldn) {
+void select_ldn(unsigned char ldn)
+{
     outb(LDNREG, REG);
     outb(ldn, VAL);
 }
 
-unsigned char read_reg(unsigned char reg) {
+unsigned char read_reg(unsigned char reg)
+{
     outb(reg, REG);
     return inb(VAL);
 }
 
-void write_reg(unsigned char reg, unsigned char val) {
+void write_reg(unsigned char reg, unsigned char val)
+{
     outb(reg, REG);
     outb(val, VAL);
 }
 
-void exit_conf_mode() {
+void exit_conf_mode()
+{
     outb(0x02, REG);
     outb(0x02, VAL);
-    if (iopl(0)) {
+    if (iopl(0))
+    {
         fprintf(stderr, "Can't drop IO permissions\n");
         exit(1);
     }
 }
 
-void cmd_on() {
+void cmd_on()
+{
     enter_conf_mode();
     select_ldn(GPIOLDN);
     write_reg(ADR, 0x1F);
@@ -62,22 +71,31 @@ void cmd_on() {
     exit_conf_mode();
 }
 
-void cmd_off() {
+void cmd_off()
+{
     enter_conf_mode();
     select_ldn(GPIOLDN);
     write_reg(ADR, 0x1E);
     exit_conf_mode();
 }
 
-void cmd_blink(char* interval) {
+void cmd_blink(char *interval)
+{
     unsigned char val;
-    if (strcmp(interval, "slow") == 0) {
+    if (strcmp(interval, "slow") == 0)
+    {
         val = 0x04;
-    } else if (strcmp(interval, "medium") == 0) {
+    }
+    else if (strcmp(interval, "medium") == 0)
+    {
         val = 0x02;
-    } else if (strcmp(interval, "fast") == 0) {
+    }
+    else if (strcmp(interval, "fast") == 0)
+    {
         val = 0x00;
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Invalid interval\n");
         exit(1);
     }
@@ -89,34 +107,54 @@ void cmd_blink(char* interval) {
     exit_conf_mode();
 }
 
-void cmd_status() {
+void cmd_status()
+{
     enter_conf_mode();
     select_ldn(GPIOLDN);
     unsigned char val = read_reg(CMDF9);
     exit_conf_mode();
-
-    if (val == 0x01) {
+    value = read_req(CMDF8) & bit0;
+    printf("value: %x\n", value);
+    if (value == bit0)
+    {
         printf("Power LED is on\n");
-    } else if (val == 0x04) {
-        printf("Power LED is blinking slowly\n");
-    } else if (val == 0x02) {
-        printf("Power LED is blinking medium\n");
-    } else if (val == 0x00) {
-        printf("Power LED is blinking fast\n");
-    } else {
-        printf("Power LED is %x (unknown)\n", val);
+    }
+    else if (value == 0)
+    {
+        printf("Power LED is off\n");
+    }
+    else
+    {
+        if (val == 0x04)
+        {
+            printf("Power LED is blinking slowly\n");
+        }
+        else if (val == 0x02)
+        {
+            printf("Power LED is blinking medium\n");
+        }
+        else if (val == 0x00)
+        {
+            printf("Power LED is blinking fast\n");
+        }
+        else
+        {
+            printf("Power LED is %x (unknown)\n", val);
+        }
     }
 }
 
 //----------------------------------n100 end / 1235u start--------------------------------
 void OemEcIbFree()
 {
-    if (iopl(3)) {
+    if (iopl(3))
+    {
         fprintf(stderr, "Can't get IO permissions\n");
         exit(1);
     }
     unsigned char Status;
-    do {
+    do
+    {
         Status = inb(CMD66);
         outb(CMD80, 0xEB);
     } while (Status & 2);
@@ -125,20 +163,24 @@ void OemEcIbFree()
 void OemEcObFull()
 {
     unsigned char Status;
-    do {
+    do
+    {
         Status = inb(CMD66);
         outb(CMD80, 0xEC);
     } while (!(Status & 1));
 }
 
-void uexit_conf_mode() {
-    if (iopl(0)) {
+void uexit_conf_mode()
+{
+    if (iopl(0))
+    {
         fprintf(stderr, "Can't drop IO permissions\n");
         exit(1);
     }
 }
 
-unsigned char uread_reg(unsigned char reg) {
+unsigned char uread_reg(unsigned char reg)
+{
     OemEcIbFree();
     outb(CMD80, CMD66);
     OemEcIbFree();
@@ -147,7 +189,8 @@ unsigned char uread_reg(unsigned char reg) {
     return inb(CMD62);
 }
 
-void uwrite_reg(unsigned char cmd, unsigned char adr,unsigned char val) {
+void uwrite_reg(unsigned char cmd, unsigned char adr, unsigned char val)
+{
     OemEcIbFree();
     outb(cmd, CMD66);
     OemEcIbFree();
@@ -156,94 +199,144 @@ void uwrite_reg(unsigned char cmd, unsigned char adr,unsigned char val) {
     outb(val, CMD62);
 }
 
-void ucmd_on() {
-    uwrite_reg(CMD81,ADR,0x00);
+void ucmd_on()
+{
+    uwrite_reg(CMD81, ADR, 0x00);
     uexit_conf_mode();
 }
-void ucmd_off() {
-    uwrite_reg(CMD81,ADR,0x01);
+void ucmd_off()
+{
+    uwrite_reg(CMD81, ADR, 0x01);
     uexit_conf_mode();
 }
 
-void ucmd_blink(char* interval) {
+void ucmd_blink(char *interval)
+{
     unsigned char val;
-    if (strcmp(interval, "slow") == 0) {
+    if (strcmp(interval, "slow") == 0)
+    {
         val = 0x02;
-    } else if (strcmp(interval, "medium") == 0) {
+    }
+    else if (strcmp(interval, "medium") == 0)
+    {
         val = 0x04;
-    } else if (strcmp(interval, "fast") == 0) {
+    }
+    else if (strcmp(interval, "fast") == 0)
+    {
         val = 0x08;
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Invalid interval\n");
         exit(1);
     }
-    uwrite_reg(CMD81,ADR,val);
+    uwrite_reg(CMD81, ADR, val);
     uexit_conf_mode();
 }
 
-void ucmd_status() {
+void ucmd_status()
+{
     unsigned char val = uread_reg(CMD81);
 
-    if (val == 0x00) {
+    if (val == 0x00)
+    {
         printf("Power LED is on\n");
-    } else if (val == 0x02) {
+    }
+    else if (val == 0x02)
+    {
         printf("Power LED is blinking slowly\n");
-    } else if (val == 0x04) {
+    }
+    else if (val == 0x04)
+    {
         printf("Power LED is blinking medium\n");
-    } else if (val == 0x08) {
+    }
+    else if (val == 0x08)
+    {
         printf("Power LED is blinking fast\n");
-    } else {
+    }
+    else if (val == 0x01)
+    {
+        printf("Power LED is off\n");
+    }
+    else
+    {
         printf("Power LED is %x (unknown)\n", val);
     }
 }
 //----------------------------------1235u end--------------------------------
 
-int main(int argc, char** argv) {
-    if (geteuid() != 0) {
+int main(int argc, char **argv)
+{
+    if (geteuid() != 0)
+    {
         fprintf(stderr, "The program is not running with root privileges. Please run as root.\n");
         exit(1);
     }
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "Usage: %s <project> <command> [args]\n", argv[0]);
         exit(1);
     }
 
-    if (strcmp(argv[1], "pro") == 0) {
+    if (strcmp(argv[1], "pro") == 0)
+    {
 
-        if (strcmp(argv[2], "on") == 0) {
+        if (strcmp(argv[2], "on") == 0)
+        {
             ucmd_on();
-        } else if (strcmp(argv[2], "blink") == 0) {
-            if (argc < 4) {
+        }
+        else if (strcmp(argv[2], "blink") == 0)
+        {
+            if (argc < 4)
+            {
                 fprintf(stderr, "Usage: %s blink <interval>\n", argv[0]);
                 exit(1);
             }
             ucmd_blink(argv[3]);
-        } else if (strcmp(argv[2], "status") == 0) {
+        }
+        else if (strcmp(argv[2], "status") == 0)
+        {
             ucmd_status();
-        } else if(strcmp(argv[2], "off") == 0) {
+        }
+        else if (strcmp(argv[2], "off") == 0)
+        {
             ucmd_off();
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "Invalid command: %s\n", argv[2]);
             exit(1);
         }
 
         return 0;
-    } else {
+    }
+    else
+    {
 
-        if (strcmp(argv[2], "on") == 0) {
+        if (strcmp(argv[2], "on") == 0)
+        {
             cmd_on();
-        } else if (strcmp(argv[2], "blink") == 0) {
-            if (argc < 4) {
+        }
+        else if (strcmp(argv[2], "blink") == 0)
+        {
+            if (argc < 4)
+            {
                 fprintf(stderr, "Usage: %s blink <interval>\n", argv[0]);
                 exit(1);
             }
             cmd_blink(argv[3]);
-        } else if (strcmp(argv[2], "status") == 0) {
+        }
+        else if (strcmp(argv[2], "status") == 0)
+        {
             cmd_status();
-        } else if (strcmp(argv[2], "off") == 0) {
+        }
+        else if (strcmp(argv[2], "off") == 0)
+        {
             cmd_off();
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "Invalid command: %s\n", argv[2]);
             exit(1);
         }
